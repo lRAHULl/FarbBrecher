8492bcec2ed488a1164e423a279425294ee70f31
killerxd999 <vishnusid98@gmail.com>
2019-09-30 16:21:17 +0530

    FB-4001

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,4 @@
 let canvas = document.getElementById("canvas");
-// window.onload = function () {
-//     startTimer();
-// };
-
-// let play = document.getElementById("play");
-
-// play.addEventListener("click", startTimer);
 
 const startTimer = () => {
     let noOfMinutes = 1;
@@ -53,41 +46,76 @@ function checkBallAlgorithm() {
         }
     }
 
-
     console.log(colorMatrix);
     let currentCandyClass = currentId.className;
     colorMatrix.forEach(colorRow => {
         colorRow.forEach(candy => {
             if (candy.className === currentCandyClass) {
-                candy.className = "ball " + differentBallTypes[Math.floor(Math.random() * 5)];
+                candy.remove();
+                refill();
                 score += 1;
             }
         })
     })
-    console.log(score);
-    // currentId.className = "ball " + differentBallTypes[Math.floor(Math.random() * 5)];
+    console.log("score" + score);
+    currentId.className = "ball " + differentBallTypes[Math.floor(Math.random() * 5)];
 }
 
 let noOfBalls = 49;
+let noOfBallsRow = 7;
 let differentBallTypes = ["red-ball", "blue-ball", "green-ball", "yellow-ball", "purple-ball"];
 
 window.onload = () => {
-    generateCandies();
+    generateCandiesRandom();
 }
 
+const refill = () => {
+    let flexAll = document.querySelectorAll(".flex-column");
+    console.log(flexAll);
+    for (let flexInteger = 0; flexInteger < flexAll.length; flexInteger++) {
+        let divAll = document.querySelectorAll(`#flex-column-${flexInteger}>div`);
+        AddNewDiv(document.getElementById(`flex-column-${flexInteger}`), divAll.length);
+    }
+    giveNewId();
+}
 
-
-const generateCandies = () => {
-    document.querySelectorAll('.ball').forEach(div => div.remove());
-
-    for (let divGenerator = 0; divGenerator < noOfBalls; divGenerator++) {
+const giveNewId = () => {
+    let divId = 0;
+    let divBlocks = document.getElementsByClassName("ball");
+    for (let divInteger = 0; divInteger < divBlocks.length; divInteger++) {
+        divBlocks[divInteger].id = divId++;
+    }
+}
+const AddNewDiv = (flexObject, length) => {
+    if (length < 7) {
         let div = document.createElement("div");
         div.className = "ball " + differentBallTypes[Math.floor(Math.random() * 5)];
-        div.id = divGenerator + 1;
-        div.onclick = checkBallAlgorithm;
+        div.id = divCount++;
+        div.addEventListener('click', checkBallAlgorithm);
+        flexObject.appendChild(div);
+    }
+}
+
+let divCount = 0;
+let flexColumnId = 0;
+const generateCandiesRandom = () => {
+    document.querySelectorAll(".ball").forEach(div => div.remove());
+
+    for (let divRow = 0; divRow < 7; divRow++) {
+        let div = document.createElement("div");
+        div.id = `flex-column-${flexColumnId++}`;
+        div.className = "flex-column"
+        for (let divColumn = 0; divColumn < 7; divColumn++) {
+            let innerDiv = document.createElement("div");
+            innerDiv.className = "ball " + differentBallTypes[Math.floor(Math.random() * 5)];
+            innerDiv.id = divCount++;
+            innerDiv.addEventListener('click', checkBallAlgorithm);
+            div.appendChild(innerDiv);
+        }
         canvas.appendChild(div);
     }
 }
+
 
 let replay = document.getElementById('replay');
 replay.addEventListener('click', generateCandies);
